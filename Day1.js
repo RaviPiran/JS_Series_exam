ffc03947d5ceff0b590461c1019861e851a311be
Piranavan <piranavanpiran2000@gmail.com>
2023-10-26 10:39:41 +0530

    add

---
+++ Day1.js
@@ -0,0 +1,114 @@
+//Q1
+let obj= [];
+console.log(obj);
+
+//Q2
+let person={
+name:  "Piran",
+age :23
+ }
+console.log(person);
+
+
+
+
+
+//Q3
+
+
+let greetStudent="Piranavan"
+
+console.log( "Hello"+" "+greetStudent+" "+"WelcometotheCodingSchool");
+
+
+//Q4
+
+let Player={
+no1:"kohli",
+no2:"smith",
+no3:"warner"
+}
+
+console.log(Player.no2);
+console.log(Player["no1"]);
+
+//Q5
+person.email = "john@exaple.com"
+console.log(person);
+
+
+//Q6
+delete person.age;
+console.log(person);
+
+//Q7
+
+
+
+
+
+
+
+
+
+
+
+
+//Q8
+let student="Piranavan";
+let course="Webdevelopment";
+// let studentcourse=student+course;
+console.log(student,course);
+
+//Q9
+
+const propertyToCheck = "address";
+
+if (propertyToCheck.it is correct) {
+  console.log();
+} else {
+  console.log(`${propertyToCheck} not found`);
+}
+
+
+
+
+
+
+
+
+//Q10
+const book = {
+    title: "The Great world",
+    author: "Lokesh",
+    year: 1969,
+  };
+  
+  const bookJSON = JSON.stringify(book);
+  
+  console.log(bookJSON);
+
+
+  //Q11
+  
+
+
+
+  const students = [
+    {
+      name: "Piran",
+      age: 23,
+      gpa: 3.5,
+    },
+    {
+      name: "kaiber",
+      age: 25,
+      gpa: 3.2,
+    },
+    {
+      name: "surenya",
+      age: 22,
+      gpa: 3.8,
+    },
+  ];
+  
